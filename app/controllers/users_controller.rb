750f6e58e79167949e5d3806702bad561a70b762
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-04-17 08:29:46 +0000

    [Finish] 応用課題7c

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
-    # @following_users = @user.following_user
-    # @follower_users = @user.follower_user
   end
 
   def index
